9eb7d4b414820a37cee3259f8d6639d7a671141b
Ashnaali3255 <ashnaalipk@gmail.com>
2023-05-05 15:35:14 +0500

    Create nav.js

---
+++ src/Components/nav.js
@@ -0,0 +1,15 @@
+import '../App.css';
+
+const Nav = () => (
+  <nav className="navBar">
+    <h1>Math Magicians</h1>
+    <ul>
+      <li><a href="/">Home</a></li>
+      <li><a href="/calcultor">Calculator</a></li>
+      <li><a href="/quote">Quotes</a></li>
+    </ul>
+
+  </nav>
+);
+
+export default Nav;
