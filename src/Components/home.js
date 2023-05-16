52d87f9afd234eea19d7aa89c0cbd63e6701f076
Ashnaali3255 <ashnaalipk@gmail.com>
2023-05-16 18:09:22 +0500

    Create home.js and add more code

---
+++ src/Components/home.js
@@ -0,0 +1,27 @@
+import '../App.css';
+import React from 'react';
+
+const Home = () => (
+  <div className="homePage">
+    <h2>Welcome to Math Magician Website!</h2>
+
+    <p>
+      Calculators are used to perform mathematical calculations. It has become an essential tool
+      in  everyday life for students, professionals, and anyone
+      who needs to perform calculations quickly and accurately.
+      Calculators can perform a range of functions, from simple arithmetic operations like
+      addition, subtraction, multiplication, and division to complex mathematical equations
+      and scientific calculations.
+    </p>
+
+    <p>
+      The basic calculator has a numeric keypad and a display screen, while more
+      advanced calculators may have additional features like graphing capabilities,
+      programmability, and scientific functions.
+      With the advent of smartphones, calculators have also been integrated into
+      mobile devices, making them more accessible than ever before.
+    </p>
+  </div>
+);
+
+export default Home;
