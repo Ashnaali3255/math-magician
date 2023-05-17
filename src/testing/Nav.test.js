722f5a5fbe14b8b64a21c809e9bd7d04b361f637
Ashnaali3255 <ashnaalipk@gmail.com>
2023-05-17 21:25:18 +0500

    Update test files

--- src/testing/Nav.test.js
+++ src/testing/Nav.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+
 it('check if the Navbar section renders properly', () => {
-const tree = renderer.create(<nav />).toJSON();
+  const tree = renderer.create(<nav />).toJSON();
   expect(tree).toMatchSnapshot();
 });
