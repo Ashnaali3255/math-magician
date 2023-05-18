17c5c8d282d2925f307c3788c5fef646f9d51c76
Ashnaali3255 <ashnaalipk@gmail.com>
2023-05-18 13:24:57 +0500

    Update file

--- src/testing/Calculator.test.js
+++ src/testing/Calculator.test.js
@@ -1,8 +1,1 @@
-import React from 'react';
-import renderer from 'react-test-renderer';
-import Calculator from '../Components/Calculator';
 
-it('check if the Calculator renders properly', () => {
-  const tree = renderer.create(<Calculator />).toJSON();
-  expect(tree).toMatchSnapshot();
-});
