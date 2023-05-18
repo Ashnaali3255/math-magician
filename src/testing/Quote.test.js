28cc4cb50ebed3ac01b6666727dc637aa502f7ee
Dave <davidolanrewaju538@gmail.com>
2023-05-18 10:11:15 +0100

    Update Quote.test.js

--- src/testing/Quote.test.js
+++ src/testing/Quote.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import Quotes from '../Components/quote';
 
+// This test file checks if the Quotes component is properly rendered in the DOM
 it('check if the Quote section renders properly', () => {
   const tree = renderer.create(<Quotes />).toJSON();
   expect(tree).toMatchSnapshot();
