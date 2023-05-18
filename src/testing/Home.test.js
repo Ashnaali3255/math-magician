7b7397087da385bedbd908a4738a41bc0077de56
Dave <davidolanrewaju538@gmail.com>
2023-05-18 10:09:24 +0100

    Update Home.test.js

--- src/testing/Home.test.js
+++ src/testing/Home.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import Home from '../Components/home';
 
+// This test checks if the Home component is properly rendered in the DOM
 it('check if the Home section renders properly', () => {
   const tree = renderer.create(<Home />).toJSON();
   expect(tree).toMatchSnapshot();
