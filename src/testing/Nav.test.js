dfa5313bcd1059266977854c814dcb53f921fc87
Dave <davidolanrewaju538@gmail.com>
2023-05-18 10:10:20 +0100

    Update Nav.test.js

--- src/testing/Nav.test.js
+++ src/testing/Nav.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 
+// This test fole checks if the Navigation is properly rendered on the DOM
 it('check if the Navbar section renders properly', () => {
   const tree = renderer.create(<nav />).toJSON();
   expect(tree).toMatchSnapshot();
