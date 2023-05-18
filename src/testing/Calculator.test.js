0a09a1eebc59acdc9eb9dacdd09a26fc2db3a4d8
Ashnaali3255 <ashnaalipk@gmail.com>
2023-05-18 13:45:12 +0500

    Update calculator .test.js file

--- src/testing/Calculator.test.js
+++ src/testing/Calculator.test.js
@@ -1,6 +1,8 @@
-//This test checks if the calculator renders in the DOM
+import React from 'react';
+import renderer from 'react-test-renderer';
+import Calculator from '../Components/Calculator';
+// This test checks if the calculator renders in the DOM
 it('check if the Calculator renders properly', () => {
   const tree = renderer.create(<Calculator />).toJSON();
   expect(tree).toMatchSnapshot();
 });
-
