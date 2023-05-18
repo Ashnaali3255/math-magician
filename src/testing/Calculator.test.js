534ca80dccd4fc8898f6346ede8d7ba627b76c61
Dave <davidolanrewaju538@gmail.com>
2023-05-18 09:25:11 +0100

    Update Calculator.test.js

--- src/testing/Calculator.test.js
+++ src/testing/Calculator.test.js
@@ -2,7 +2,8 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import Calculator from '../Components/Calculator';
 
-// it('check if the Calculator renders properly', () => {
-//   const tree = renderer.create(<Calculator />).toJSON();
-//   expect(tree).toMatchSnapshot();
-// });
+//This test checks if the calculator renders in the DOM
+it('check if the Calculator renders properly', () => {
+  const tree = renderer.create(<Calculator />).toJSON();
+  expect(tree).toMatchSnapshot();
+});
