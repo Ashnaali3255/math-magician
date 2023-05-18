7faed8d3a2f670613a542c7db9c05ed672327355
Ashnaali3255 <ashnaalipk@gmail.com>
2023-05-18 18:20:32 +0500

    Update calcultor.test.js

--- src/testing/Calculator.test.js
+++ src/testing/Calculator.test.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import Calculator from '../Components/Calculator';
+import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react';
+
 // This test checks if the calculator renders in the DOM
 it('check if the Calculator renders properly', () => {
   const tree = renderer.create(<Calculator />).toJSON();
   expect(tree).toMatchSnapshot();
+});
+
+test('Check if buttons are rendered in the DOM', () => {
+  render(<Calculator />);
+  expect(screen.getByText('AC')).toBeInTheDocument();
+  expect(screen.getByText('9')).toBeInTheDocument();
 });
